4265f328094120962a29f5b842e3d06dc869d792
imashaRan12 <imasharandima26@gmail.com>
2026-03-22 02:36:09 +0530

    Creating the Authoriztion

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
-import connectToDatabase from "./database/monodb.js";
+import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
